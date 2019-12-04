d08d07af126c4f57826aa37e31bb0a6d9092c93d
EdUTroN <edtronco@gmail.com>
2019-12-04 18:24:54 -0300

    Mejora en publicaciones
    - Se ajusta paso de datos
    - Se cambia versión
    - Se elimina hora de cierre de lista

--- client/templates/events/event-edit.js
+++ client/templates/events/event-edit.js
@@ -30,10 +30,10 @@ Template.eventEdit.events({
         var name = $('#event-name').val();
         var fecha = moment($('#input-evento-fecha').val(), 'DD/MM/YY').toDate()
 
-        var parts = $('#event-close-time').val().split(":")
+        /*var parts = $('#event-close-time').val().split(":")
         var hours = Number(parts[0])
         hours = hours + 12 * (hours < 12 ? 1 : -1)
-        var closeTime = hours * 60 * 60 * 1000 + Number(parts[1]) * 60 * 1000
+        var closeTime = hours * 60 * 60 * 1000 + Number(parts[1]) * 60 * 1000*/
 
         var eventSelected = Session.get('EventoSeleccionado');
 
@@ -43,7 +43,6 @@ Template.eventEdit.events({
                 userId: Meteor.userId(),
                 name: name,
                 date: fecha,
-                closeTime: closeTime,
                 total: 0,
                 arrives: 0,
                 averageCheckTime: 0
@@ -52,8 +51,7 @@ Template.eventEdit.events({
             Events.update(eventSelected._id, {
                 $set: {
                     'name': name,
-                    'date': fecha,
-                    'closeTime': closeTime
+                    'date': fecha
                 }
             });
         }
--- server/publish.js
+++ server/publish.js
@@ -1,61 +1,74 @@
 FechaEventoActual = function () {
-    var fecha = moment()
-    if (fecha.get('hour') < 12) fecha.subtract(1, 'd')
-    return fecha.startOf('day').hours(0)
+  var fecha = moment()
+  if (fecha.get('hour') < 12) fecha.subtract(1, 'd')
+  return fecha.startOf('day').hours(0)
 }
 
 Meteor.publish('eventos.porIds', function (ids) {
-    check(ids, [String]);
-    return Events.find({
-        _id: {
-            $in: ids
-        }
-    });
+  check(ids, [String]);
+  return Events.find({
+    _id: {
+      $in: ids
+    }
+  });
 });
 
 Meteor.publish('eventos', function () {
-    return Events.find({}, {
-        limit: 6,
-        sort: {
-            date: -1
-        }
-    });
+  return Events.find({}, {
+    limit: 6,
+    fields: {
+      created: false,
+      userId: false,
+      closeTime: false,
+      averageCheckTime: false
+    },
+    sort: {
+      date: -1
+    }
+  });
 });
 
 Meteor.publishComposite('asistentes', function (eventoId, pagina) {
-    let saltos = 20;
-    return {
-        find: function () {
-            return Events.find({
-                _id: eventoId
-            });
-        },
-        children: [{
-            find: function (e) {
-                return Attenders.find({
-                    eventId: e._id
-                }, { 
-                    skip: saltos * pagina, 
-                    limit: saltos 
-                });
-            },
-            children: [{
-                find: function (a) {
-                    return Guests.find({
-                        _id: a.guestId
-                    });
-                }
+  let saltos = 20;
+  return {
+    find: function () {
+      return Events.find({
+        _id: eventoId
+      });
+    },
+    children: [{
+      find: function (e) {
+        return Attenders.find({
+          eventId: e._id
+        }, {
+          skip: saltos * pagina,
+          limit: saltos
+        });
+      },
+      children: [{
+        find: function (a) {
+          return Guests.find({
+            _id: a.guestId
+          });
+        }
       }]
     }]
-    }
+  }
 });
 
-Meteor.publish("baneados", function() {
-    return Guests.find({ baneado: true });
+Meteor.publish("baneados", function () {
+  return Guests.find({
+    baneado: true
+  });
 });
 
-Meteor.publish("top10", function() {
-    return Guests.find({}, { sort: { asistencias: 1 }, limit: 20 });
+Meteor.publish("top10", function () {
+  return Guests.find({}, {
+    sort: {
+      asistencias: 1
+    },
+    limit: 20
+  });
 })
 
 /*
@@ -66,162 +79,164 @@ Meteor.publish("top10", function() {
  *
  * */
 Meteor.publish('users', function () {
-    if (!Meteor.userId()) return [];
-    var rol = Meteor.user().profile.role;
-    return Meteor.users.find({
-        "profile.role": {
-            $gte: rol
-        }
-    }, {
-        fields: {
-            services: false,
-            createdAt: false
-        }
-    });
+  if (!Meteor.userId()) return [];
+  var rol = Meteor.user().profile.role;
+  return Meteor.users.find({
+    "profile.role": {
+      $gte: rol
+    }
+  }, {
+    fields: {
+      services: 0,
+      createdAt: 0
+    }
+  });
 });
 
 Meteor.publishComposite("eventoActual", {
-    find: function () {
-        var fecha = FechaEventoActual()
-        var desde = moment(fecha).toDate()
-        var hasta = moment(fecha).add(24, 'h').toDate()
-        return Events.find({
-            date: {
-                $gte: desde,
-                $lt: hasta
-            }
-        });
+  find: function () {
+    var fecha = FechaEventoActual()
+    var desde = moment(fecha).toDate()
+    var hasta = moment(fecha).add(24, 'h').toDate()
+    return Events.find({
+      date: {
+        $gte: desde,
+        $lt: hasta
+      }
+    });
+  },
+  children: [{
+    find: function (evento) {
+      return Attenders.find({
+        eventId: evento._id
+      }, {
+        sort: {
+          fecha: 1
+        }
+      });
     },
     children: [{
-        find: function (evento) {
-            return Attenders.find({
-                eventId: evento._id
-            }, {
-                sort: {
-                    fecha: 1
-                }
-            });
-        },
-        children: [{
-            find: function (ra) {
-                return Guests.find({
-                    _id: ra.guestId
-                })
-            }
+      find: function (ra) {
+        return Guests.find({
+          _id: ra.guestId
+        })
+      }
     }]
   }]
 });
 
 Meteor.publishComposite('efectividad', function (desde, hasta) {
-    return {
-        find: function () {
-            return Events.find({
-                date: {
-                    $gte: desde
-                },
-                date: {
-                    $lte: hasta
-                }
-            });
+  return {
+    find: function () {
+      return Events.find({
+        date: {
+          $gte: desde
         },
-        children: [{
-            find: function (e) {
-                return Attenders.find({
-                    eventId: e._id
-                });
-            },
-            children: [{
-                find: function (a) {
-                    return Guests.find({
-                        _id: a.guestId
-                    });
-                }
+        date: {
+          $lte: hasta
+        }
+      });
+    },
+    children: [{
+      find: function (e) {
+        return Attenders.find({
+          eventId: e._id
+        });
+      },
+      children: [{
+        find: function (a) {
+          return Guests.find({
+            _id: a.guestId
+          });
+        }
       }]
     }]
-    }
+  }
 });
 
 Meteor.publish('birps', function (desde, hasta) {
-    // Busca eventos entre las fechas
-    var eventosIds = Events.find({
-        $and: [{
-            date: {
-                $gte: desde
-            }
+  // Busca eventos entre las fechas
+  var eventosIds = Events.find({
+    $and: [{
+      date: {
+        $gte: desde
+      }
     }, {
-            date: {
-                $lte: hasta
-            }
+      date: {
+        $lte: hasta
+      }
     }]
-    }).map(function (o) {
-        return o._id;
-    });
-    var rpIds = Meteor.users.find({
-        "profile.role": {
-            $in: [1, 2, 3]
+  }).map(function (o) {
+    return o._id;
+  });
+  var rpIds = Meteor.users.find({
+    "profile.role": {
+      $in: [1, 2, 3]
+    }
+  }).map(function (u) {
+    return u._id;
+  });
+  // Hace arreglo de llaves con 
+  return BIRPs.find({
+    $and: [
+      {
+        eventoId: {
+          $in: eventosIds
         }
-    }).map(function (u) {
-        return u._id;
-    });
-    // Hace arreglo de llaves con 
-    return BIRPs.find({
-        $and: [
-            {
-                eventoId: {
-                    $in: eventosIds
-                }
             },
-            {
-                rpId: {
-                    $in: rpIds
-                }
+      {
+        rpId: {
+          $in: rpIds
+        }
             },
-            {
-                asisten: {
-                    $gt: 0
-                }
+      {
+        asisten: {
+          $gt: 0
+        }
             }
     ]
-    });
+  });
 });
 
 Meteor.publishComposite('birpevento', function (eventoId) {
-    return {
-        find: function() {
-            return BIRPs.find({
-                eventoId: eventoId
-            });
-        },
-        children: [{
-            find: (function(reg) {
-                return Meteor.users.find({ _id: reg.rpId }, {
-                    fields: {
-                        services: false,
-                        createdAt: false,
-                        emails: false,
-                        username: false,
-                        "profile.role": false,
-                        "profile.isRPAdmin": false,
-                        "profile.rut": false
-                    }
-                })
-            })
-        }]
-    }
+  return {
+    find: function () {
+      return BIRPs.find({
+        eventoId: eventoId
+      });
+    },
+    children: [{
+      find: (function (reg) {
+        return Meteor.users.find({
+          _id: reg.rpId
+        }, {
+          fields: {
+            services: 0,
+            createdAt: 0,
+            emails: 0,
+            username: 0,
+            "profile.role": 0,
+            "profile.isRPAdmin": 0,
+            "profile.privilegiado": 0
+          }
+        })
+      })
+    }]
+  }
 });
 
 Meteor.publish('biactual', function () {
-    var fecha = FechaEventoActual()
-    var desde = moment(fecha).toDate()
-    var hasta = moment(fecha).add(24, 'h').toDate()
-    var evento = Events.findOne({
-        date: {
-            $gte: desde,
-            $lt: hasta
-        }
-    });
-    if (!evento) return [];
-    return BIRPs.find({
-        eventoId: evento._id
-    });
+  var fecha = FechaEventoActual()
+  var desde = moment(fecha).toDate()
+  var hasta = moment(fecha).add(24, 'h').toDate()
+  var evento = Events.findOne({
+    date: {
+      $gte: desde,
+      $lt: hasta
+    }
+  });
+  if (!evento) return [];
+  return BIRPs.find({
+    eventoId: evento._id
+  });
 });
